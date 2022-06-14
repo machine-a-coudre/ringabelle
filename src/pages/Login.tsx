de3b56bfb3fd4d5d6486680d4ecffab8794e021d
Sew <sew@pepetrek.com>
2022-06-14 21:08:33 +0200

    Update Login page with new form inputs and i18n

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,7 +7,7 @@ export default function Login({ onLogin }: { onLogin: Function }) {
 
   return (
     <div className="flex h-full min-h-screen w-full">
-      <div className="bg-gray-500 grow text-white">
+      <div className="bg-gray-400 grow text-gray-500">
         <div className="p-11 h-full flex flex-col items-center justify-end justify-items-stretch">
           Ringabell bell bell...
         </div>
@@ -23,13 +23,46 @@ export default function Login({ onLogin }: { onLogin: Function }) {
               The bell is strange. Please ringabell and make it ring again and again and again.
             </p>
             <form>
+              <label htmlFor="login" className="mt-4 block">
+                {t('Login.form.login.label')}
+                <input id="login" type="text" className="my-2 border-2 border-gray-300 rounded-md w-full p-2" placeholder="name@ringabell.com" />
+              </label>
+
+              <label htmlFor="password" className="mt-2 block">
+                {t('Login.form.password.label')}
+                <input id="password" type="password" className="my-2 border-2 border-gray-300 rounded-md w-full p-2" placeholder="••••••••••" />
+              </label>
+
+              <p className="text-right">
+                <a
+                  href="https://www.ringabell.org"
+                  title={t('Login.form.forgotPassword.a.title')}
+                  className="text-xs underline text-blue-400"
+                >
+                  {t('Login.form.forgotPassword')}
+
+                </a>
+              </p>
+
               <button
-                className="bg-gray-700 text-white my-9 px-4 py-2 w-full rounded-md"
+                className="bg-gray-700 text-white my-8 px-4 py-2 w-full rounded-md"
                 type="button"
                 onClick={onClickHandler}
               >
                 Login
               </button>
+
+              <p className="text-xs">
+                {t('Login.form.noAccount')}
+                <a
+                  href="https://www.ringabell.org"
+                  title={t('Login.form.noAccount.a.title')}
+                  className="ml-1 text-xs underline text-blue-400"
+                >
+                  {t('Login.form.signIn')}
+
+                </a>
+              </p>
             </form>
           </main>
         </div>
